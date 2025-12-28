4964a5dfaa662682ffc94c029330cde3e3aa6f5a
matheuspereirasalvador <matheuspereirasalvador14@gmail.dom>
2025-12-28 13:50:44 -0300

    ci: adicionando pipeline do github actions

--- tests/test_carrinho.py
+++ tests/test_carrinho.py
@@ -1,6 +1,7 @@
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 
+
 def test_adicionar_item_ao_carrinho(driver):
     login_page = LoginPage(driver)
     inventory_page = InventoryPage(driver)
@@ -9,6 +10,7 @@ def test_adicionar_item_ao_carrinho(driver):
     login_page.fazer_login("standard_user", "secret_sauce")
 
     inventory_page.adicionar_mochila_ao_carrinho()
-    inventory_page.adicionar_bicicleta_ao_carrinho()
+    assert "1" == inventory_page.obter_quantidade_carrinho()
 
+    inventory_page.adicionar_bicicleta_ao_carrinho()
     assert "2" == inventory_page.obter_quantidade_carrinho()
