2152ff68c0805f9d99fb27ee8c61b814ee64d036
matheuspereirasalvador <matheuspereirasalvador14@gmail.dom>
2025-12-28 13:54:51 -0300

    fix: adicionando explicit wait para o badge do carrinho

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,23 +1,30 @@
 from selenium.webdriver.common.by import By
+# IMPORTS NOVOS PARA O WAIT EXPLICITO
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class InventoryPage:
     def __init__(self, driver):
         self.driver = driver
 
         self.backpack_add_button = (By.ID, "add-to-cart-sauce-labs-backpack")
-        self.bikeLight_add_button = (By.ID, "add-to-cart-sauce-labs-bike-light")
+        self.bike_light_add_button = (By.ID, "add-to-cart-sauce-labs-bike-light")
         self.cart_badge = (By.CLASS_NAME, "shopping_cart_badge")
         self.page_title = (By.CLASS_NAME, "title")
 
-    def adicionar_mochila_ao_carrinho(self):
+    def adicionar_mochila(self):
         self.driver.find_element(*self.backpack_add_button).click()
 
-
-    def adicionar_bicicleta_ao_carrinho(self):
-        self.driver.find_element(*self.bikeLight_add_button).click()
+    def adicionar_bike_light(self):
+        self.driver.find_element(*self.bike_light_add_button).click()
 
     def obter_quantidade_carrinho(self):
-        return self.driver.find_element(*self.cart_badge).text
+        wait = WebDriverWait(self.driver, 10)
+
+        elemento = wait.until(EC.visibility_of_element_located(self.cart_badge))
+
+        return elemento.text
 
     def obter_titulo_pagina(self):
         return self.driver.find_element(*self.page_title).text
